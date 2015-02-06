a10ca254baec89af0881761ff90480d9b8a7d8a1
coltonfitzgerald <rockincolton@gmail.com>
2015-02-06 10:09:45 -0800

    shout out ot maggie and colton

---
+++ assets/js/tutorial-magnets.js
@@ -0,0 +1,118 @@
+var points = 0;
+$(document).ready(function(){    
+    // target elements with the "draggable" class
+    interact('.draggable')
+      .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+
+        // call this function on every dragmove event
+        onmove: function (event) {
+          var target = event.target,
+              // keep the dragged position in the data-x/data-y attributes
+              x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+              y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+          // translate the element
+          target.style.webkitTransform =
+          target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+          // update the posiion attributes
+          target.setAttribute('data-x', x);
+          target.setAttribute('data-y', y);
+        },
+        // call this function on every dragend event
+        onend: function (event) {
+          var textEl = event.target.querySelector('p');
+
+          textEl && (textEl.textContent =
+            'moved a distance of '
+            + (Math.sqrt(event.dx * event.dx +
+                         event.dy * event.dy)|0) + 'px');
+        }
+      });
+      interact('.tapedbattery-drop').dropzone({
+          // only accept elements matching this CSS selector
+          accept: '#tapedbattery',
+          // Require a 75% element overlap for a drop to be possible
+          overlap: 0.95,
+
+          // listen for drop related events:
+
+          ondropactivate: function (event) {
+            // add active dropzone feedback
+            event.target.classList.add('drop-active');
+          },
+          ondragenter: function (event) {
+            var draggableElement = event.relatedTarget,
+                dropzoneElement = event.target;
+
+            // feedback the possibility of a drop
+            dropzoneElement.classList.add('drop-target');
+            draggableElement.classList.add('can-drop');
+          },
+          ondragleave: function (event) {
+            // remove the drop feedback style
+            event.target.classList.remove('drop-target');
+            event.relatedTarget.classList.remove('can-drop');
+            --points;  
+          },
+          ondrop: function (event) {
+            ++points;
+            console.log(points);
+            if (points == 3){
+            }
+          },
+          ondropdeactivate: function (event) {
+            // remove active dropzone feedback
+            event.target.classList.remove('drop-active');
+            event.target.classList.remove('drop-target');
+          }
+      });
+      interact('.magnet-drop').dropzone({
+          // only accept elements matching this CSS selector
+          accept: '#magnetstack',
+          // Require a 75% element overlap for a drop to be possible
+          overlap: 0.95,
+
+          // listen f or drop related events:
+
+          ondropactivate: function (event) {
+            // add active dropzone feedback
+            event.target.classList.add('drop-active');
+          },
+          ondragenter: function (event) {
+            var draggableElement = event.relatedTarget,
+                dropzoneElement = event.target;
+
+            // feedback the possibility of a drop
+            dropzoneElement.classList.add('drop-target');
+            draggableElement.classList.add('can-drop');
+          },
+          ondragleave: function (event) {
+            // remove the drop feedback style
+            event.target.classList.remove('drop-target');
+            event.relatedTarget.classList.remove('can-drop');
+            --points;  
+          },
+          ondrop: function (event) {
+            ++points;
+            console.log(points);
+            if (points == 3){
+            }
+          },
+          ondropdeactivate: function (event) {
+            // remove active dropzone feedback
+            event.target.classList.remove('drop-active');
+            event.target.classList.remove('drop-target');
+          }
+      });
+});
+  
